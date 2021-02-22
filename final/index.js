5457aa6727e064cd3cc7a07d5c28b2545776905c
franktranvantu <59758589+franktranvantu@users.noreply.github.com>
2021-02-22 17:35:35 +0700

    Update

--- final/index.js
+++ final/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import {render} from 'react-dom';
 import {Provider} from 'react-redux';
-import {createStore} from "redux";
+import {createStore} from 'redux';
 import PlayerReducer from './src/reducers/player';
 import Scoreboard from './src/containers/Scoreboard';
 
 const store = createStore(PlayerReducer);
 
 render(
-  <Provider store={store}>
-    <Scoreboard />
-  </Provider>,
+  // <Provider store={store}>
+    <Scoreboard />,
+  // </Provider>,
   document.getElementById('root')
 );
