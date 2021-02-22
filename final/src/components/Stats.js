554914096537ec3a033a66bffcc55294e55a0182
franktranvantu <59758589+franktranvantu@users.noreply.github.com>
2021-02-22 17:39:48 +0700

    Update

--- final/src/components/Stats.js
+++ final/src/components/Stats.js
@@ -1,6 +1,10 @@
 import React, {PropTypes} from 'react';
 
-const Stats = props => {
+const Stats = props => {const playerCount = props.players.length;
+  const totalPoints = props.players.reduce(function(total, player) {
+    return total + player.score;
+  }, 0);
+
   return (
     <table className="stats">
       <tbody>
